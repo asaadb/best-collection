7d176a9919dcd1d5ccf83c3925ba8e914200c876
Asaad Budayr <asad_289@hotmail.com>
2022-12-17 16:05:24 +0300

    feat: create initial nav bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-
+import React from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      E-commerce site "Best Collection"
-    </div>
-  );
+    <Box>
+        <Navbar/>
+        <Routes>
+            <Route  path="/" element={<Home/>}/>
+        </Routes>
+    </Box>
+  )
 }
 
-export default App;
+export default App
