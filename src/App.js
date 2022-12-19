711831cba76b60980e9b182633c9329a1f444591
Asaad Budayr <asad_289@hotmail.com>
2022-12-19 20:08:17 +0300

    feat: left the products state and fetching logic to App

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@ import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
+import ProductDetail from './pages/ProductDetail';
+import Cart from './pages/Cart';
+import { useState, useEffect } from 'react';
 
 const App = () => {
+  let [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+        .then(res => res.json())
+        .then(data => setProducts(data))
+        .catch(err => console.log('ERROR OCCURED WHILE FETCHING PRODUCTS:', err));
+  }, [])
+
   return (
     <Box>
         <Navbar/>
         <Routes>
-            <Route  path="/" element={<Home/>}/>
+            <Route  path="/" element={<Home products={products}/>}/>
+            <Route path='/products/:id' element={<ProductDetail/>}/>
+            <Route path='/cart' element={<Cart products={products}/>}/>
         </Routes>
     </Box>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,16 +3,8 @@ import Hero from '../components/Hero';
 import Products from '../components/Products';
 import { useEffect, useState } from 'react';
 
-const Home = () => {
-  let [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    fetch('https://fakestoreapi.com/products')
-        .then(res => res.json())
-        .then(data => setProducts(data))
-        .catch(err => console.log('ERROR OCCURED WHILE FETCHING PRODUCTS:', err))
-  }, [])
-
+const Home = ({products}) => {
+  
   return (
     <>
         <Hero />
